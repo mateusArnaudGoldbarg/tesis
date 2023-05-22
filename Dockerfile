06f91e3648be18cbcaba0673e4de167d5d1a062b
Mateus Arnaud Goldbarg <42541080+mateusArnaudGoldbarg@users.noreply.github.com>
2023-05-22 11:36:58 -0300

    Add files via upload

---
+++ Dockerfile
@@ -0,0 +1,17 @@
+# Use a base image with Python and required dependencies
+FROM python:3.9
+
+# Set the working directory inside the container
+WORKDIR /app
+
+# Copy the h5 file to the container
+COPY data.h5 /app/data.h5
+
+# Install any additional dependencies if needed
+# RUN pip install flask numpy h5py
+
+# Copy your application code to the container
+COPY main.py /app/main.py
+
+# Set the command to run your application
+CMD [ "python", "/app/main.py" ]
--- main.py
+++ main.py
@@ -1,18 +1,44 @@
 # import main Flask class and request object
 from flask import Flask, request
-import tensorflow as tf
+#import tensorflow as tf
 import numpy as np
+import h5py
 
 # create the Flask app
 app = Flask(__name__)
 
-model = tf.keras.models.load_model('test.h5')
+#data = h5py.File('data.h5', 'r')
+#d = data['tensors']
+#model = d[()]
+
+#open model
+model__ = []
+names__ = []
+
+# open the file as 'f'
+with h5py.File('data.h5', 'r') as f:
+    data = f['tensors']
+    #print(data.members)
+    members = list(data.keys())
+    #print("Members in the h5 file:")
+    for member in members:
+        #print(member)
+        d = data[member]
+        for m in list(d.keys()):
+          dd = d[m]
+          m_ = dd[()]
+          model__.append(m_)
+          names__.append(member+"/"+m)
+
+
+
+#model = tf.keras.models.load_model('test.h5')
 
 @app.route('/')
 def home_page():
     return '''<h1>HOME PAGE</h1>'''
 
-@app.route('/model-input',methods=['POST'])
+@app.route('/model-input')
 def model_input():
     # if key doesn't exist, returns None
     #inp = request.args.get('inp')
@@ -21,14 +47,15 @@ def model_input():
 
     data = request.get_json()
     #data['w'] = int(data['w'])
-    my_array = np.array(data['w'],dtype=np.float32)
+    #my_array = np.array(data['w'],dtype=np.float32)
 
-    print(my_array.shape)
-    my_array = my_array.reshape(1,32,32,3)
-    print(my_array.shape)
-    p = model.predict(my_array)
-    print(p)
-    return '''<h1>I am {}% sure that this is {}</h1>'''.format(p.max()*100, p.argmax())
+    #print(my_array.shape)
+    #my_array = my_array.reshape(1,32,32,3)
+    #print(my_array.shape)
+    #p = model.predict(my_array)
+    #print(p)
+    #return '''<h1>I am {}% sure that this is {}</h1>'''.format(p.max()*100, p.argmax())
+    return '''<h1>{}</h1><p>{}</p>'''.format(names__[1],model__[1])
 
 
 if __name__ == '__main__':
